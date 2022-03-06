73461d48ed0b48cf0304d1aa0d91d871f1b971c8
Carlos Almanzar <castroenmanuel98@gmail.com>
2022-03-06 17:53:46 -0400

    Navigator Information functionality created

--- Menu.js
+++ Menu.js
@@ -9,3 +9,13 @@ const message = document.getElementById('calc');
 message.addEventListener("click", function() {
   window.alert('Coming Soon');
 });
+
+let navInfo = `
+Browser Code Name: ${navigator.appCodeName}
+Browser Name: ${navigator.appName}
+Browser Version: ${navigator.appVersion}
+Browser Platform: ${navigator.platform}
+Browser Language: ${navigator.language}
+`;
+
+console.log(navInfo);

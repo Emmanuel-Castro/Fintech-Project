933c60d1be592c4eee07bd885726fd49c4befca6
Carlos Almanzar <castroenmanuel98@gmail.com>
2022-04-22 02:25:58 -0400

    JS code edited

--- Currency_Converter.js
+++ Currency_Converter.js
@@ -1,11 +1,11 @@
 
-    var getCurrencySelect = document.getElementById('currencySelect');
-    var getCurrencyConvert = document.getElementById('currencyConvert');
-    var getQuantity = document.getElementById('quantity');
+    let getCurrencySelect = document.getElementById('currencySelect');
+    let getCurrencyConvert = document.getElementById('currencyConvert');
+    let getQuantity = document.getElementById('quantity');
 
     function convertProcess(select, convert, quantity) {
       //Last update, March 31, 2021
-      var currencies = {
+      let currencies = {
         USD: 1.00,
         EUR: 0.85,
         GBP: 0.72,
@@ -22,37 +22,10 @@
 
     function convertResult() {
 
-      var result = convertProcess(getCurrencySelect.value, getCurrencyConvert.value, getQuantity.value).toFixed(2);
-      var total = `${getQuantity.value} ${getCurrencySelect.value} = ${result} ${getCurrencyConvert.value}`;
+      let result = convertProcess(getCurrencySelect.value, getCurrencyConvert.value, getQuantity.value).toFixed(2);
+      let total = `${getQuantity.value} ${getCurrencySelect.value} <br> ${result} ${getCurrencyConvert.value}`;
 
       document.getElementById('resultCont').innerHTML = total;
     }
 
     document.getElementById('btn').addEventListener("click", convertResult);
-
-/*
-    var separator = document.getElementById('quantity');
-
-    separator.addEventListener('keyup', (evnt) => {
-
-        var inputData = evnt.target.value.split(',').join('');
-        inputData = inputData.split('').reverse();
-
-        var outputData = [];
-        var aux = '';
-
-        var divide = Math.ceil(inputData.length / 3);
-
-        for (let x = 0; x < divide; x++) {
-          for (let i = 0; i < 3; i++) {
-            if (inputData[i + (x*3)] != undefined) {
-              aux += inputData[i + (x*3)];
-            }
-          }
-          outputData.push(aux);
-          aux = '';
-          evnt.target.value = outputData.join(',').split('').reverse().join('');
-        }
-
-    }, false)
-*/
